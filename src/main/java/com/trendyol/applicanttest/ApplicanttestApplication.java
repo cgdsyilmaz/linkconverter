0709a07abff4537e85d7a250039c1d76e31a7341
Cagdas <Yilmaz>
2021-03-08 23:43:25 +0300

    [TASK-1]
    Initialized the service using spring initializer, created the database and added postgresql properties to the application.properties file.

---
+++ src/main/java/com/trendyol/applicanttest/ApplicanttestApplication.java
@@ -0,0 +1,13 @@
+package com.trendyol.applicanttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicanttestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicanttestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trendyol/applicanttest/ApplicanttestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trendyol.applicanttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicanttestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
